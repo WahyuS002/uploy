8836ba46f3786156dda69cf7253ad765f178820c
Wahyu Syahputra <44608522+WahyuS002@users.noreply.github.com>
2026-03-01 16:57:56 +0700

    feat: add dockerPsHandler api into main.go

--- api/main.go
+++ api/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+)
+
+func dockerPsHandler(w http.ResponseWriter, r *http.Request) {
+	out, err := exec.Command("docker", "ps").Output()
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Write(out)
+}
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/api/docker/ps", dockerPsHandler)
+	fmt.Println("Server berjalan di localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
