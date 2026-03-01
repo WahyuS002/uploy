81443b5a5cbcd0254fc58a0ef4d7f72b735bb509
Wahyu Syahputra <44608522+WahyuS002@users.noreply.github.com>
2026-03-01 17:20:36 +0700

    feat: dockerNginxHandler()
    we got issue where the frontend need to wait ~10s until the output is
    shown

--- api/main.go
+++ api/main.go
@@ -17,8 +17,20 @@ func dockerPsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(out)
 }
 
+func dockerNginxHandler(w http.ResponseWriter, r *http.Request) {
+	out, err := exec.Command("docker", "pull", "nginx:latest").Output()
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Write(out)
+}
+
 func main() {
 	http.HandleFunc("/api/docker/ps", dockerPsHandler)
+	http.HandleFunc("/api/docker/nginx", dockerNginxHandler)
 	fmt.Println("Server berjalan di localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-	let output = 'Klik button diatas untuk fetch docker ps';
+	let output = 'Klik button diatas untuk pull nginx:latest';
 
-	async function fetchDockerPs() {
-		const res = await fetch('/api/docker/ps');
+	async function fetchDockerNginx() {
+		const res = await fetch('/api/docker/nginx');
 		output = await res.text();
 	}
 </script>
 
 <h1>Hello World (uploy)</h1>
 
-<button class="cursor-pointer rounded-md bg-black px-2 py-2 text-white" onclick={fetchDockerPs}>Fetch Docker Ps</button>
+<button class="cursor-pointer rounded-md bg-black px-2 py-2 text-white" onclick={fetchDockerNginx}
+	>Fetch Nginx (pull)</button
+>
 <p>{output}</p>
